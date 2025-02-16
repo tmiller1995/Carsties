28ee288f3d5ffbfbd98d53f5bce48b1e32b003a6
Tyler Miller <tyler.e.miller1995@gmail.com>
2025-02-16 12:47:04 -0600

    Refactor Tailwind setup and update dependencies.
    Replaced individual Tailwind directives with a unified @import statement for clarity. Updated Tailwind CSS and other dependencies to their latest versions, ensuring compatibility and improving build performance. Removed unused packages for a cleaner dependency tree.

--- src/react-frontend/app/auctions/AuctionCard.tsx
+++ src/react-frontend/app/auctions/AuctionCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from "next/image";
 import {Auction} from "@/app/auctions/Auction";
+import CountdownTimer from "@/app/auctions/CountdownTimer";
 
 type Props = {
     auction: Auction
@@ -12,10 +13,21 @@ export default function AuctionCard({auction}: Props) {
             <div className={`relative w-full bg-gray-200 aspect-video rounded-lg overflow-hidden`}>
                 <Image src={auction.imageUrl}
                        alt={`Image of ${auction.make} ${auction.model} in ${auction.color}`}
-                       width={150}
-                       height={150}
+                       fill={true}
+                       sizes={`(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw)`}
+                       priority={true}
                        className={`object-cover`} />
+                <div className={`absolute bottom-2 left-2`}>
+                    <CountdownTimer auctionEnd={auction.auctionEnd} />
+                </div>
             </div>
+            <div className={`flex justify-between items-center mt-4`}>
+                <h3 className={`text-gray-700`}>{auction.make} {auction.model}</h3>
+                <p className={`font-semibold text-sm`}>
+                    {auction.year}
+                </p>
+            </div>
+
         </a>
     )
 }
