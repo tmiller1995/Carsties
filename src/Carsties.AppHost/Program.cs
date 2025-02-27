5688f9eadfd87dba364df701372a665f5c1d0c7a
Tyler Miller <tyler.e.miller1995@gmail.com>
2025-02-26 22:12:44 -0600

    Add project dependencies and configure service integrations
    Updated `Directory.Packages.props` and `Carsties.AppHost.csproj` to include new package versions and project references for PostgreSQL, RabbitMQ, RavenDB, and NodeJS hosting. Enhanced `Program.cs` to configure service integrations for AuctionService_API, SearchService_API, IdentityService, and GatewayService, establishing dependencies and wait conditions for proper initialization. Prepared frontend application with npm integration and Docker file publishing.

--- src/Carsties.AppHost/Program.cs
+++ src/Carsties.AppHost/Program.cs
@@ -1,3 +1,39 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var postgres = builder.AddPostgres("postgres");
+var rabbitMq = builder.AddRabbitMQ("rabbitmq");
+var ravenDb = builder.AddRavenDB("ravendb");
+
+var auctionServiceApi = builder.AddProject<AuctionService_API>("auction-service")
+    .WithReference(postgres)
+    .WaitFor(postgres)
+    .WithReference(rabbitMq)
+    .WaitFor(rabbitMq);
+
+var searchServiceApi = builder.AddProject<SearchService_API>("search-service")
+    .WithReference(ravenDb)
+    .WaitFor(ravenDb)
+    .WithReference(rabbitMq)
+    .WaitFor(rabbitMq);
+
+var identityService = builder.AddProject<IdentityService>("identity-service")
+    .WithReference(postgres)
+    .WaitFor(postgres);
+
+var gatewayService = builder.AddProject<GatewayService>("gateway-service")
+    .WithReference(identityService)
+    .WaitFor(identityService)
+    .WithReference(auctionServiceApi)
+    .WaitFor(auctionServiceApi)
+    .WithReference(searchServiceApi)
+    .WaitFor(searchServiceApi);
+
+builder.AddNpmApp("frontend", @"..\react-frontend")
+    .WithReference(gatewayService)
+    .WaitFor(gatewayService)
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
 builder.Build().Run();
