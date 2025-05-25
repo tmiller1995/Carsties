af8deee57c14ac627ee71c691737fac4c149bfe3
Tyler Miller <tyler.e.miller1995@gmail.com>
2025-05-25 10:05:24 -0500

    Add NextAuth for authentication with Duende IdentityServer
    Integrated NextAuth to enable authentication and session management using Duende IdentityServer. Updated `package.json` and `bun.lock` to include required dependencies, and implemented relevant routes and configuration in `auth.ts`.

---
+++ src/react-frontend/app/auth.ts
@@ -0,0 +1,16 @@
+import NextAuth, { Profile } from "next-auth";
+import DuendeIDS6Provider from "next-auth/providers/duende-identity-server6";
+import { OIDCConfig } from "@auth/core/providers";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    DuendeIDS6Provider({
+      id: "id-server",
+      clientId: "frontend",
+      clientSecret: "SuperSecretDooDooDoo",
+      issuer: "http://localhost:7018",
+      authorization: { params: { scope: "openid profile auctionApp" } },
+      idToken: true,
+    } as OIDCConfig<Profile>),
+  ],
+});
